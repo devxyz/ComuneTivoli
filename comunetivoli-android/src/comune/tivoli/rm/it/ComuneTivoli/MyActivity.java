09ec8a65dceeb6edf084228ca25928f535cfdedb
millozzi.stefano <stefano.millozzi@gmail.com>
2016-03-15 16:05:31 +0100

    xxx

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MyActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MyActivity.java
@@ -11,5 +11,9 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+
+
     }
 }
