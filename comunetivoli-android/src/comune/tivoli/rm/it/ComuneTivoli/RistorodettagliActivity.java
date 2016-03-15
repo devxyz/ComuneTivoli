85d1f1dbbb57ab448b7ca0b8df769c930ea72eca
millozzi.stefano <stefano.millozzi@gmail.com>
2016-03-15 15:38:19 +0100

    stefano

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/RistorodettagliActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/RistorodettagliActivity.java
@@ -10,6 +10,8 @@ public class RistorodettagliActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.ristorodettagli_activity);
     }
 }
