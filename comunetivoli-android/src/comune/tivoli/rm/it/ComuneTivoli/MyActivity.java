4df780cc209bfa161b86ebd1c009a7aa23d037f0
millozzi.stefano <stefano123$>
2016-04-12 16:01:25 +0200

    erfghrtf

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MyActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MyActivity.java
@@ -1,16 +1,33 @@
 package comune.tivoli.rm.it.ComuneTivoli;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    ImageView myactivity_imageView ;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        myactivity_imageView =  (ImageView )findViewById(R.id.myactivity_imageView );
+        myactivity_imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MyActivity.this,HomeActivity.class);
+                startActivity(i);
+
+            }
+        });
 
     }
 }
