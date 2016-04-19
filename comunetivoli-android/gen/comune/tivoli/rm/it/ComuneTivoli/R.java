9ce4af93040568a2a57d704b2aee0e08ac7f3451
millozzi.stefano <stefano123$>
2016-04-19 15:52:48 +0200

    aaa

--- comunetivoli-android/gen/comune/tivoli/rm/it/ComuneTivoli/R.java
+++ comunetivoli-android/gen/comune/tivoli/rm/it/ComuneTivoli/R.java
@@ -4,4 +4,6 @@ package comune.tivoli.rm.it.ComuneTivoli;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
+    public class Myhome {
+    }
 }
