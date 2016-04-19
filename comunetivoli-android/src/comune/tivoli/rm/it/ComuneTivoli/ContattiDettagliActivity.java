58ad756e89228abec0ef2211c93821ae83ff1803
millozzi.stefano <stefano.millozzi@gmail.com>
2016-04-19 16:42:40 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	comunetivoli-android/comunetivoli-android.iml

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/ContattiDettagliActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/ContattiDettagliActivity.java
@@ -2,6 +2,7 @@ package comune.tivoli.rm.it.ComuneTivoli;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -10,6 +11,10 @@ import android.widget.TextView;
 public class ContattiDettagliActivity extends Activity {
     TextView dettagli_titolo;
     TextView dettagli_descrizione;
+    ImageButton btn_chiama;
+    ImageButton btn_maps;
+    ImageButton btn_email;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,7 @@ public class ContattiDettagliActivity extends Activity {
         dettagli_descrizione = (TextView)findViewById(R.id.descrizione_dettagli);
         dettagli_titolo.setText(getIntent().getExtras().getString("titolo"));
         dettagli_descrizione.setText(getIntent().getExtras().getString("descrizione"));
+
     }
 
 }
