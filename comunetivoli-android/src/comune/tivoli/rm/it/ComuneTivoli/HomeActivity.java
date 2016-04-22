5afb87809d53360149925bac7446ee4fbb49eed8
millozzi.stefano <stefano.millozzi@gmail.com>
2016-04-22 11:36:40 +0200

    Contatti Dettagli Activity

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/HomeActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/HomeActivity.java
@@ -2,11 +2,16 @@ package comune.tivoli.rm.it.ComuneTivoli;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 /**
  * Created by millozzi.stefano on 19/04/2016.
  */
 public class HomeActivity extends Activity {
+    ImageButton btn_turismo;
+    ImageButton btn_eventi;
+    ImageButton btn_news;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
