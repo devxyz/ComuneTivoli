076ca616692318596499baca2549aa8495139575
devxyz <stefano.millozzi@gmail.com>
2016-04-22 11:11:00 +0200

    ccccc

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MonumentiDettagli2Activity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/MonumentiDettagli2Activity.java
@@ -3,10 +3,8 @@ package comune.tivoli.rm.it.ComuneTivoli;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageButton;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.TextView;
-import comune.tivoli.rm.it.ComuneTivoli.util.MonumentiUtil;
 
 /**
  * Created by millozzi.stefano on 15/03/2016.
@@ -26,14 +24,12 @@ public class MonumentiDettagli2Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.monumentidettagli_activity);
-        title_text= (TextView) findViewById(R.id.title_txt);
-        dettagli_text= (TextView) findViewById(R.id.txt_description);
-        image_monumento= (ImageView) findViewById(R.id.monumenti_image);
-        web_btn=(ImageButton) findViewById(R.id.web_btn);
-        tred_btn = (ImageButton) findViewById( R.id.tred_btn);
-        maps_btn=(ImageButton) findViewById(R.id.maps_btn);
-        
-
+        title_text = (TextView) findViewById(R.id.title_txt);
+        dettagli_text = (TextView) findViewById(R.id.txt_description);
+        image_monumento = (ImageView) findViewById(R.id.monumenti_image);
+        web_btn = (ImageButton) findViewById(R.id.web_btn);
+        tred_btn = (ImageButton) findViewById(R.id.tred_btn);
+        maps_btn = (ImageButton) findViewById(R.id.maps_btn);
 
 
     }
