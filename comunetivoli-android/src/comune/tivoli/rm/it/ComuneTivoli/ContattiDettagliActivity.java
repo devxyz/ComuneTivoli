89d5d158efeaa033afeab99057ee754fe9befd50
millozzi.stefano <stefano.millozzi@gmail.com>
2016-04-26 15:26:49 +0200

    Contatti Dettagli Activity maps

--- comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/ContattiDettagliActivity.java
+++ comunetivoli-android/src/comune/tivoli/rm/it/ComuneTivoli/ContattiDettagliActivity.java
@@ -2,13 +2,13 @@ package comune.tivoli.rm.it.ComuneTivoli;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import comune.tivoli.rm.it.ComuneTivoli.util.TemplateUtil;
 
 /**
  * Created by millozzi.stefano on 19/04/2016.
@@ -20,15 +20,12 @@ public class ContattiDettagliActivity extends Activity {
     ImageButton btn_maps;
     ImageButton btn_email;
     ImageView screen;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contattidettagli_activity_decorated);
-        TemplateUtil.inizializzaComponentiTemplate(this, "Contatti");
-
+        setContentView(R.layout.contattidettagli_activity);
         dettagli_titolo = (TextView) findViewById(R.id.titolo_dettagli);
-        dettagli_descrizione = (TextView) findViewById(R.id.descrizione_dettagli);
+        dettagli_descrizione = (TextView)findViewById(R.id.descrizione_dettagli);
         dettagli_titolo.setText(getIntent().getExtras().getString("titolo"));
         dettagli_descrizione.setText(getIntent().getExtras().getString("descrizione"));
         btn_chiama = (ImageButton) findViewById(R.id.btn_chiama_dettagli);
@@ -39,31 +36,22 @@ public class ContattiDettagliActivity extends Activity {
         btn_chiama.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent;
+                Intent callIntent ;
                 callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:" + getIntent().getExtras().getString("telefono")));
+                callIntent.setData(Uri.parse("tel:"+getIntent().getExtras().getString("telefono")));
                 startActivity(callIntent);
             }
         });
-
-        screen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn_maps.callOnClick();
-            }
-        });
-
         btn_maps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mapsIntent;
-                String mapsLabel;
-                String mapsGen;
-                mapsGen = getIntent().getExtras().getString("maps");
+                String mapsLabel = getIntent().getExtras().getString("titolo") ;
+                String mapsGen  = getIntent().getExtras().getString("maps");
+                String[] coordinate = mapsGen.split(",");
+
                 mapsIntent = new Intent(android.content.Intent.ACTION_VIEW,
-                        Uri.parse(mapsGen));
-                //mapsIntent = new Intent(android.content.Intent.ACTION_VIEW,
-                //      Uri.parse("geo:0,0?q="+lat+","+lng+"&z=16 (" + maplLabel + ")"));
+                      Uri.parse("geo:0,0?q="+coordinate[0]+","+coordinate[1]+"&z=16 (" + mapsLabel + ")"));
                 startActivity(mapsIntent);
             }
         });
